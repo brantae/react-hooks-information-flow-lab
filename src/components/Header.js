c09649dd9b6b339f10b535e4150d619d2985d9ac
Anslie <brantae@gmail.com>
2022-11-05 18:23:30 -0500

    commit tree 9f403f7a1081ed444e3858c1773c9b0f58c081e7

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Header( { isDarkMode, onDarkModeClick } ) {
+
+   // const { isDarkMode, onDarkModeClick } = props
+     
+    return (
+       
+            <header>
+                <h2>Shopster</h2>
+                <button onClick={onDarkModeClick}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+                </button>
+            </header>
+       
+        )
+}
+
+export default Header
